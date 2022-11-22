eb297e48f61a5cbe842ff360c668e80dcf970394
killer050488 <107902349+killer050488@users.noreply.github.com>
2022-11-22 23:25:32 +0800

    Add files via upload

--- test.py
+++ test.py
@@ -1,5 +1,19 @@
-import time
+from datetime import datetime
+from time import sleep
+import logging
 
-while True:
-    print('hi')
-    time.sleep(1)
+Log_Format = "%(levelname)s %(asctime)s - %(message)s"
+
+logging.basicConfig(filename = "mylog.log",
+                    filemode = "w",
+                    format = Log_Format,
+                    level = logging.INFO)
+logger = logging.getLogger()
+logger.error("Our First Log Message")
+
+while(1):
+    sleep(5)
+
+    print("print " + str(datetime.now().strftime('%Y-%m-%d %H:%M:%S')))
+    logger.info(str(datetime.now().strftime('%Y-%m-%d %H:%M:%S')))
+    # print(1/0) # 輸出錯誤時使用
